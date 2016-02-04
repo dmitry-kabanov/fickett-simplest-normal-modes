66024b8689c0d26770a2efdb08c0879c600992a3
Dmitry Kabanov <kabanovdmitry@gmail.com>
2016-02-04 16:12:13 +0300

    Improve minimization function

--- leestewart/minimize_boundedness_function.m
+++ leestewart/minimize_boundedness_function.m
@@ -1,4 +1,4 @@
-function [result] = minimize_boundedness_function(guess, M, params, znd_sol)
+function [result] = minimize_boundedness_function(guess, M, params)
 % Minimize boundedness function $H(\alpha)$, using `guess`.
 % 
 % Function $H(\alpha)$ is complex-valued, therefore, we minimize its
@@ -9,6 +9,10 @@ function [result] = minimize_boundedness_function(guess, M, params, znd_sol)
 % ----------
 % guess : struct with fields: [alpha_re, alpha_im]
 %     Initial guess on $\alpha$.
+% M : float
+%     Domain length.
+% params : struct
+%     Parameters of the problem.
 % 
 % Returns
 % -------
@@ -18,7 +22,7 @@ alpha_re = guess.alpha_re;
 alpha_im = guess.alpha_im;
 
 % Use builtin MATLAB function for unconstrained nonlinear optimization.
-fun = @(x) abs(compute_boundedness_function(x(1) + 1j * x(2), M, params, znd_sol));
-opts = optimset('TolX', 1e-8, 'TolFun', 1e-8);
+fun = @(x) abs(compute_boundedness_function(x(1) + 1j * x(2), M, params));
+opts = optimset('TolX', 1e-10, 'TolFun', 1e-10, 'Display', 'iter');
 [x, fval, exitflag, output] = fminsearch(fun, [alpha_re, alpha_im], opts)
 end
