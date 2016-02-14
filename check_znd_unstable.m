e0cbd41aa575adec5ef091904eede9312495d745
Dmitry Kabanov <kabanovdmitry@gmail.com>
2016-02-14 16:46:59 +0300

    Improve script that checks correctness of ZND solution

--- check_znd_unstable.m
+++ check_znd_unstable.m
@@ -1,7 +1,8 @@
 % Check that we compute ZND structure correctly when we use function
 % `compute_znd_data_at_point`.
-close all;
-clear;
+close all
+clear all
+clc
 
 % Physical free parameters.
 q = 1.7; theta = 2.4;
@@ -10,23 +11,29 @@ q = 1.7; theta = 2.4;
 params = compute_aux_params(q, theta);
 
 % Lambda tolerance, that is, closeness of lambda to unity.
-lambda_tol = 1e-4;
+lambda_tol = 1e-6;
 
-lambda = linspace(0, 1-lambda_tol, 1000);
-u = zeros('like', lambda);
-du_dx = zeros('like', lambda);
-du_dl = zeros('like', lambda);
-dl_dx = zeros('like', lambda);
-dw_du = zeros('like', lambda);
-dw_dl = zeros('like', lambda);
-w = zeros('like', lambda);
+N = 10000;
+
+lambda = linspace(0, 1-lambda_tol, N)';
+u      = zeros(N, 1);
+du_dx  = zeros(N, 1);
+du_dl  = zeros(N, 1);
+dl_dx  = zeros(N, 1);
+dw_du  = zeros(N, 1);
+dw_dl  = zeros(N, 1);
+w      = zeros(N, 1);
+x      = zeros(N, 1);
 
 for i = 1:length(lambda)
     znd = compute_znd_data_at_point(lambda(i), params);
 
     u(i) = znd.u;
     du_dl(i) = znd.du_dl;
+    du_dx(i) = znd.du_dx;
+    dl_dx(i) = znd.dl_dx;
     dw_du(i) = znd.dw_du;
     dw_dl(i) = znd.dw_dl;
     w(i) = znd.w;
+    x(i) = compute_x_at_point(lambda(i), params);
 end
