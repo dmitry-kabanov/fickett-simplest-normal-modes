c051e5882e461e4ad84a6e0f9147ad8d1168b445
Dmitry Kabanov <kabanovdmitry@gmail.com>
2016-02-14 16:54:43 +0300

    Change parameters

--- test_compute_boundedness_function.m
+++ test_compute_boundedness_function.m
@@ -4,11 +4,13 @@ clear
 % Physical free parameters.
 q = 1.7; theta = 2.4;
 
-% Domain length.
-M = 13;
+% Domain length. This domain length corresponds to the lambda_tol=1e-6.
+M = 12.317732705436374;
 
 % Struct with free and dependent parameters.
 params = compute_aux_params(q, theta);
 
-alpha = 3.269e-2 + 1j * 6.636e-1;
+alpha_re = 3.2689475021629215e-02;
+alpha_im = 6.6361473999774434e-01;
+alpha = alpha_re + 1j * alpha_im;
 H = compute_boundedness_function(alpha, M, params);
