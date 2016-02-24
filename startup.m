78412204005cb77de0d0aa0aeff54c7dc19bed53
Dmitry Kabanov <kabanovdmitry@gmail.com>
2016-02-24 18:27:44 +0300

    Add startup.m to specify default settings for figures

---
+++ startup.m
@@ -0,0 +1,8 @@
+% Set default properties for figures.
+% See http://www.mathworks.com/help/matlab/ref/startup.html
+
+% Default linewidth.
+set(0, 'defaultLineLineWidth', 1.5);
+
+set(0, 'defaultFigureColor', 'white');
+set(0, 'defaultAxesFontSize', 12);
