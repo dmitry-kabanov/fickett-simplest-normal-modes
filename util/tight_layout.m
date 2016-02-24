66db4bbe6047a98721197eb01c9531e7242abf42
Dmitry Kabanov <kabanovdmitry@gmail.com>
2016-02-24 19:22:14 +0300

    Add util function `tight_layout`
    This function allows to expand current plot such that margins are
    minimal

---
+++ util/tight_layout.m
@@ -0,0 +1,13 @@
+function tight_layout
+% Tight layout.
+% See http://www.mathworks.com/help/matlab/creating_plots/save-figure-with-minimal-white-space.html
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
+end
+
